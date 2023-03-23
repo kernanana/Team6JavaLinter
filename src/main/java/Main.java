eee027f47f69e35220530fcfc161ea10a493d342
rhit-gilesgc <gilesgc@rose-hulman.edu>
2023-03-23 14:09:58 -0400

    Refactor Long Methods: main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,31 +5,36 @@ import Presentation.ConsoleUI;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args){
-        ArrayList<CheckType> availibleChecks = new ArrayList<>();
-        HashMap<CheckType, Check> checkComposition = new HashMap<>();
-        availibleChecks.add(CheckType.PoorNamingConvention);
-        checkComposition.put(CheckType.PoorNamingConvention, new NamingConventionCheck());
-        availibleChecks.add(CheckType.EqualsHashCode);
-        checkComposition.put(CheckType.EqualsHashCode, new EqualsHashCodeCheck());
-        availibleChecks.add(CheckType.SingleResponsibilityPrinciple);
-        checkComposition.put(CheckType.SingleResponsibilityPrinciple, new SingleResponsibilityPrincipleCheck());
-        availibleChecks.add(CheckType.InformationHidingViolation);
-        checkComposition.put(CheckType.InformationHidingViolation, new InformationHidingCheck());
-        availibleChecks.add(CheckType.SingletonPattern);
-        checkComposition.put(CheckType.SingletonPattern, new SingletonPatternCheck());
-        availibleChecks.add(CheckType.DecoratorPattern);
-        checkComposition.put(CheckType.DecoratorPattern, new DecoratorPatternCheck());
-        availibleChecks.add(CheckType.StrategyPattern);
-        checkComposition.put(CheckType.StrategyPattern, new StrategyPatternCheck());
+    private static final List<CheckType> availableChecks = new ArrayList<>();
+    private static final Map<CheckType, Check> checkComposition = new HashMap<>();
+
+    public static void main(String[] args) {
+        initializeAvailableChecks();
         ProjectDataManager projectDataManager = new ASMProjectDataManager(new DefaultDataLoader());
         PlantClassUMLParser umlParser = new PlantClassUMLParser(new PlantUMLSourceStringReader(new SourceStringReaderCreator()), new PrintWriterUMLTextWriter());
         Linter linter = new Linter(checkComposition, projectDataManager, umlParser);
-        ConsoleUI ui = new ConsoleUI(linter, availibleChecks);
+        ConsoleUI ui = new ConsoleUI(linter, availableChecks);
         ui.UIMain();
+    }
+
+    private static void initializeAvailableChecks() {
+        registerCheck(CheckType.PoorNamingConvention, new NamingConventionCheck());
+        registerCheck(CheckType.EqualsHashCode, new EqualsHashCodeCheck());
+        registerCheck(CheckType.SingleResponsibilityPrinciple, new SingleResponsibilityPrincipleCheck());
+        registerCheck(CheckType.InformationHidingViolation, new InformationHidingCheck());
+        registerCheck(CheckType.SingletonPattern, new SingletonPatternCheck());
+        registerCheck(CheckType.DecoratorPattern, new DecoratorPatternCheck());
+        registerCheck(CheckType.StrategyPattern, new StrategyPatternCheck());
+    }
+
+    private static void registerCheck(CheckType checkType, Check check) {
+        availableChecks.add(checkType);
+        checkComposition.put(checkType, check);
     }
 
 }
