1ea010ec8f8614a7c0d8a3dc1572179eacf67608
rhit-gilesgc <gilesgc@rose-hulman.edu>
2023-03-23 14:29:40 -0400

    Refactor Long Methods: ConsoleUi.getUserOptions()

--- src/main/java/Presentation/ConsoleUI.java
+++ src/main/java/Presentation/ConsoleUI.java
@@ -8,11 +8,10 @@ import Domain.UserOptions;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ConsoleUI extends UI{
+public class ConsoleUI extends UI {
 
 
     public ConsoleUI(Linter linter, List<CheckType> availibleChecks) {
@@ -25,7 +24,7 @@ public class ConsoleUI extends UI{
                 new InputStreamReader(System.in));
         System.out.println("What checks would you like to perform?");
         for (int i = 0; i < this.availibleChecks.size(); i++){
-            System.out.println(Integer.toString(i + 1) + ". " + this.availibleChecks.get(i).toString());
+            System.out.println((i + 1) + ". " + this.availibleChecks.get(i).toString());
         }
         System.out.println("Example answer: 1,3,2");
         try {
@@ -53,39 +52,50 @@ public class ConsoleUI extends UI{
 
     private UserOptions getUserOptions(ArrayList<CheckType> checksToPerformTypes, BufferedReader reader) throws IOException {
         UserOptions userOptions = new UserOptions();
-        if (checksToPerformTypes.contains(CheckType.SingleResponsibilityPrinciple)){
-            System.out.println("You have selected to check for the Single Responsibility Principle, what would you like to set as your maximum amount of public methods?");
-            System.out.println("You may enter a number or simply enter \"default\"");
-            String maximumMethods = reader.readLine();
-            if (maximumMethods.toLowerCase().trim().equals("default")){
-                userOptions.maximumMethods = -1;
-            }else{
-                userOptions.maximumMethods = Integer.parseInt(maximumMethods);
-            }
-        }
-        if(checksToPerformTypes.contains(CheckType.PoorNamingConvention)){
-            System.out.println("You have selected to check that your project follows Standard Naming Conventions, would you like to autocorrect the names of classes/fields/methods?");
-            System.out.println("Please enter yes or no");
-            String stnReader = reader.readLine();
-            if(stnReader.toLowerCase().trim().equals("yes")){
-                userOptions.namingConventionAutoCorrect = true;
-            } else if (stnReader.toLowerCase().trim().equals("no")) {
-                userOptions.namingConventionAutoCorrect = false;
-            }
-            else {
-                System.out.println("Invalid input, autocorrect is not enabled");
-                userOptions.namingConventionAutoCorrect = false;
-            }
-        }
+        setMaximumMethods(checksToPerformTypes, reader, userOptions);
+        setNamingConventionAutoCorrect(checksToPerformTypes, reader, userOptions);
+        setParseUml(reader, userOptions);
+        return userOptions;
+    }
+
+    private void setParseUml(BufferedReader reader, UserOptions userOptions) throws IOException {
         System.out.println("Would you like to generate a uml? (yes or no)");
-        if (reader.readLine().toLowerCase().equals("yes")){
+        if (reader.readLine().equalsIgnoreCase("yes")){
             userOptions.parseUml = true;
             System.out.println("What is the directory you would like the uml image and text to be outputted to? Please enter the full file path.");
             userOptions.umlOutputDirectory = reader.readLine();
         }else{
             userOptions.parseUml = false;
         }
-        return userOptions;
+    }
+
+    private void setMaximumMethods(ArrayList<CheckType> checksToPerformTypes, BufferedReader reader, UserOptions userOptions) throws IOException {
+        if (checksToPerformTypes.contains(CheckType.SingleResponsibilityPrinciple)) {
+            System.out.println("You have selected to check for the Single Responsibility Principle, what would you like to set as your maximum amount of public methods?");
+            System.out.println("You may enter a number or simply enter \"default\"");
+            String maximumMethods = reader.readLine();
+            if (maximumMethods.toLowerCase().trim().equals("default")) {
+                userOptions.maximumMethods = -1;
+            } else {
+                userOptions.maximumMethods = Integer.parseInt(maximumMethods);
+            }
+        }
+    }
+
+    private void setNamingConventionAutoCorrect(ArrayList<CheckType> checksToPerformTypes, BufferedReader reader, UserOptions userOptions) throws IOException {
+        if (checksToPerformTypes.contains(CheckType.PoorNamingConvention)) {
+            System.out.println("You have selected to check that your project follows Standard Naming Conventions, would you like to autocorrect the names of classes/fields/methods?");
+            System.out.println("Please enter yes or no");
+            String stnReader = reader.readLine();
+            if (stnReader.toLowerCase().trim().equals("yes")) {
+                userOptions.namingConventionAutoCorrect = true;
+            } else if (stnReader.toLowerCase().trim().equals("no")) {
+                userOptions.namingConventionAutoCorrect = false;
+            } else {
+                System.out.println("Invalid input, autocorrect is not enabled");
+                userOptions.namingConventionAutoCorrect = false;
+            }
+        }
     }
 
     private void displayResults(ArrayList<PresentationInformation> presentationInformations){
