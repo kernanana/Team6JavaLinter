f9e196b78cc8335b193f0633b5cf1e595c25741d
rhit-edwardbs <edwardbs@rose-hulman.edu>
2023-04-19 17:47:29 -0400

    refactors too many assertions

--- src/test/java/InformationHidingCheckTests.java
+++ src/test/java/InformationHidingCheckTests.java
@@ -37,16 +37,18 @@ public class InformationHidingCheckTests {
 
     @Test
     public void GetterSetters(){
+        String[] setters = {"setNumber", "setString"};
+        String[] getters = {"getNumber", "getString"};
         Check check = new InformationHidingCheck();
         DefaultDataLoader dataLoader = new DefaultDataLoader();
         ProjectDataManager projectDataManager = new ASMProjectDataManager(dataLoader);
         CheckData checkData = new CheckData(projectDataManager.generateClassAdapters("./src/test/resources/GetterSetterDummyData/GetterSetter"), new UserOptions());
         PresentationInformation result = check.check(checkData);
         Assertions.assertTrue(result.hasPassed()); //false if no getters/setters or is a dataclass
-        Assertions.assertEquals(result.getDisplayLines().get(0), "setNumber in ASMPracticeCode/GetterSetterDummyData/GetterSetterClass is a Setter");
-        Assertions.assertEquals(result.getDisplayLines().get(1), "setString in ASMPracticeCode/GetterSetterDummyData/GetterSetterClass is a Setter");
-        Assertions.assertEquals(result.getDisplayLines().get(2), "getNumber in ASMPracticeCode/GetterSetterDummyData/GetterSetterClass is a Getter");
-        Assertions.assertEquals(result.getDisplayLines().get(3), "getString in ASMPracticeCode/GetterSetterDummyData/GetterSetterClass is a Getter");
+        for(String setter : setters)
+            Assertions.assertTrue(result.getDisplayLines().contains(setter + " in ASMPracticeCode/GetterSetterDummyData/GetterSetterClass is a Setter"));
+        for(String getter : getters)
+            Assertions.assertTrue(result.getDisplayLines().contains(getter + " in ASMPracticeCode/GetterSetterDummyData/GetterSetterClass is a Getter"));
         //Assertions.assertTrue(result.displayLines.size() == 0);
     }
 }
